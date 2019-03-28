d8de8e8810938095ae3bda697ff438dacf305781
Jared Reichardt <jardreichardt@gmail.com>
2019-03-28 15:46:26 -0500

    fixed typo

--- two_numbers.py
+++ two_numbers.py
@@ -2,4 +2,4 @@ num1 = float(input("Please enter first number: "))
 num2 = float(input("Please enter second number: "))
 
 if num1 > num2:
-    print("num 1 is bigger than num 2")
+    print("Number 1 is bigger than Number 2")
