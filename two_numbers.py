46f9d8aeb38fbae2ca250e4aac9bdd6201094c85
Jared Reichardt <jardreichardt@gmail.com>
2019-03-28 15:48:54 -0500

    added statement for if numbers are equal

--- two_numbers.py
+++ two_numbers.py
@@ -2,6 +2,10 @@ num1 = float(input("Please enter first number: "))
 num2 = float(input("Please enter second number: "))
 
 if num1 > num2:
-    print("num 1 is bigger than num 2")
+    print("Number 1 is bigger than Number 2")
+    
 if num2 > num1:
-    print("num 2 is bigger than num 1")
+    print("Number 2 is bigger than Number 1")
+    
+if num1 == num2:
+    print("Number 1 and Number 2 are equal")
